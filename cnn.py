3f234f565f4682bb62642cea6edfe42074fd7796
Tkht44 <kanta-takahata@azapa-eng.co.jp>
2025-07-22 13:41:46 +0900

    ignores

--- cnn.py
+++ cnn.py
@@ -2,22 +2,15 @@ import torch.nn as nn
 
 class_num = 10
 # set hyper param
-conv_1 = 3
-conv_2 = 6
-conv_3 = 16
-fc1 = 120
-fc2 = 84
-
-
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(conv_1, conv_2, 5)
+        self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)  # 半分にする
-        self.conv2 = nn.Conv2d(conv_2, conv_3, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, fc1)
-        self.fc2 = nn.Linear(fc1, fc2)
-        self.fc3 = nn.Linear(fc2, class_num)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, class_num)
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax(dim=1)
 
@@ -34,6 +27,5 @@ class CNN(nn.Module):
         x = self.fc3(x)
         x = self.softmax(x)
         return x
-
 
 net = CNN()
